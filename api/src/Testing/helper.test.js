51bff4abfe10e61501ab09fda683aaaa90deeb0e
Michalis Van Steen <michalis.van.steen@student.ehb.be>
2021-11-15 22:26:05 +0100

    Adding test & database test

--- api/src/Testing/helper.test.js
+++ api/src/Testing/helper.test.js
@@ -1,34 +1,61 @@
-const backendVariables = require("./../index");
 const { test, expect } = require("@jest/globals");
-const helpers = require("./../herlpers");
 const { help } = require("yargs");
+const helpers = require("./../database_test");
 
-const {
-    string
-} = require("yargs");
-const helpers = require("../index")
 
-test("Helper function to check a string length", () => {
-    expect(helpers.checkStringLength()).toBeFalsy();
-    expect(helpers.checkStringLength(120)).toBeFalsy();
-    expect(helpers.checkStringLength("fkgndskngf")).toBeFalsy();
-    expect(helpers.checkStringLength("hello")).toMatch("hello");
-    expect(helpers.checkStringLength("Hello")).not.toMatch("hello");
-    expect(typeof helpers.checkStringLength("hello")).toMatch("string");
+
+// const { response } = require('express');
+// const app = require('../index');
+
+const backendVariables = require("../index");
+const backendroutes = require("../index.js");
+const request = require('supertest');
+
+
+// const {
+//     string
+// } = require("yargs");
+// const helpers = require("../index")
+
+// test("Helper function to check a string length", () => {
+//     expect(helpers.checkStringLength()).toBeFalsy();
+//     expect(helpers.checkStringLength(120)).toBeFalsy();
+//     expect(helpers.checkStringLength("fkgndskngf")).toBeFalsy();
+//     expect(helpers.checkStringLength("hello")).toMatch("hello");
+//     expect(helpers.checkStringLength("Hello")).not.toMatch("hello");
+//     expect(typeof helpers.checkStringLength("hello")).toMatch("string");
+// })
+
+test("testing port length", () => {
+  expect(helpers.checkPortLength(backendVariables.port.toString())).toBeFalsy();
 })
+
 
 
 //Endpoint testing
 
-const supertest = require('supertest');
-const { response } = require('express');
-const request = supertest(app)
-const backendroutes = require("../index.js");
-
 it('Testing connection to endpoint', async() => {
-  const response = await request(apiroutes.app).get(('backend/producenten'))
+  const response = await request(backendroutes.app).get('/')
   expect(response.status).toBe(200)
-  expect(response.body.message).toBe('pass!')
   done()
 })
+
+it('Testing connection to endpoint get', async() => {
+  const response = await request(backendroutes.app).get('backend/producenten')
+  expect(response.status).toBe(200)
+  done()
+})
+
+it('Testing connection to endpoint update', async() => {
+  const response = await request(backendroutes.app).patch('backend/producentDataAanpassen/1')
+  expect(response.status).toBe(200)
+  done()
+})
+
+it('Testing connection to endpoint delete', async() => {
+  const response = await request(backendroutes.app).delete('backend/producentDataVerwijderen/1')
+  expect(response.status).toBe(200)
+  done()
+})
+
 
--- api/src/database_test.js
+++ api/src/database_test.js
@@ -1,38 +1,67 @@
-const supertest = require('supertest');
-const app = require('./../server.js')
-const pg = require('knex')({  
-    client: 'pg',  version: '9.6',
-    searchPath: ['knex', 'public'],  
-    connection: process.env.PG_CONNECTION_STRING ? process.env.PG_CONNECTION_STRING :'postgres://example:example@localhost:5432/test'
-});
-
-const request = supertest(app);
-describe('testing postgres', () => {  
-    test('full circle', async (done) =>{    
-        try {      
-            let uuid = null;      
-            await request.post('/test')        
-            .send({ title: 'test', summary: 'testing' })        
-            .expect(200)        
-            .then((resp) => resp.body.res)        
-            .then((res) => {          
-                uuid = res[0].uuid        
-            }).catch((e) => {          
-                console.log(e)        
-            })      
-            await pg.raw('BEGIN');      
-            pg.select('*').table("posts").where({uuid}).then((rows) => {        
-                console.log(rows)        
-                expect(rows).toBeInstanceOf(Array);        
-                expect(rows.length).toBe(1);      
-            })      
-            .then(() => {        
-                done();      
-            })    
-        } catch(err) {      
-            throw err;    
-        } finally {    
-
-        }  
-    })
-});
+const helpers = {
+    /**
+     * checks if string length is smaller than or equal to 10
+     * @param {string} str the user given string 
+     * @returns false if not a string or too long, otherwise string itself
+     */
+    checkPortLength(str) {
+      return str && typeof str == "string" && str.length > 4 ? str : false;
+    },
+  
+    /**
+     * check if body send by endpoint is in order
+     * @param {object} body 
+     * @returns {object} body if all is capitalised and shortened, or false if something missing
+     */
+    
+    bodyCheck(body) {
+      if(body && body.title && body.imageURL && body.excerpt) {
+        const { title, imageURL, excerpt} = body;
+        if(title.length < 10 && this.checkIfURL(imageURL)) {
+          return {
+            ...body,
+            title: this.capitalise(title),
+            excerpt: this.shortenText(excerpt, 20)
+          }
+        }
+      }
+      return false;
+    },
+    
+    /**
+     * shorten text to a certain length, add ... at the end
+     * @param {*} text input text
+     * @param {*} length desired length (account for the "...")
+     * @returns string shortened with "..." at end
+     */
+    shortenText(text, length) {
+      if(text) {
+        return text.length > length ? text.substring(0, length)+"..." : text;
+      }
+      return false;
+    },
+  
+    /**
+     * capitalise the first letter of a given string
+     * @param {string} string input string
+     * @returns the capitalised string
+     */
+    capitalise(string) {
+      return string ? string.charAt(0).toUpperCase() + string.slice(1) : false;
+    },
+  
+    /**
+     * check if a given string is a url
+     * @param {string} url 
+     * @returns true if url, false if not
+     */
+    checkIfURL(url) {
+      if(url) {
+        const regex = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+        return regex.test(url);
+      }
+      return false;
+    }
+  }
+  
+  module.exports = helpers;
